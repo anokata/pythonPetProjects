a5d21dec2b5dd2384715b7a465c3763407aae94f
anokata <quartenium@gmail.com>
2016-10-03 15:52:55 +0300

    Refactoring.

--- pong/pong.py
+++ pong/pong.py
@@ -2,9 +2,61 @@ import pyglet
 from pyglet.window import key
 import random
 
+def center_image(image):
+  image.anchor_x = image.width // 2
+  image.anchor_y = image.height // 2
+
 class Game:
   playerWins = 0
   botWins = 0
+  ball = None
+  player = None
+  bot = None
+  
+  window = None
+  score_label = None
+  
+  def __init__(self):
+    self.window = pyglet.window.Window()
+    window = self.window
+    
+    self.ball = Ball(pyglet.image.load('ball3.png'), window.width, window.height)
+    self.player = Player(window.height, pyglet.image.load('player.png'))
+    self.bot = Bot(window.height, pyglet.image.load('player.png'), window.width - 20)
+    
+    self.score_label = pyglet.text.Label(text="Player: 0  Bot: 0", x=window.width // 2, y=window.height - 30, anchor_x='center', anchor_y='center')
+    pyglet.gl.glClearColor(0.7,0.5,0.3, 1)
+    #window.push_handlers(pyglet.window.event.WindowEventLogger())
+    pyglet.clock.schedule_interval(self.mechanic, 1.0/30)
+    
+    @window.event
+    def on_draw():
+      window.clear()
+      g.score_label.draw()
+      g.ball.sprite.draw()
+      g.player.sprite.draw()
+      g.bot.sprite.draw()
+
+    @window.event
+    def on_key_press(symbol, mod):
+      if symbol == key.A:
+        print('A')
+      if symbol == key.LEFT:
+        print('left')
+      if symbol == key.UP:
+        g.player.isMove = direction.up
+      if symbol == key.DOWN:
+        g.player.isMove = direction.down
+
+    @window.event
+    def on_key_release(symbol, mod):
+      if symbol == key.UP or symbol == key.DOWN:
+        g.player.isMove = False
+
+  def mechanic(self, dt):
+    self.ball.step(dt)
+    self.player.step()
+    self.bot.randStep()
 
 class direction:
   up = 1
@@ -68,53 +120,7 @@ class Ball():
     if self.sprite.y > self.yMax:
       self.dy = - self.dy
 
-def mechanic(dt):
-  ball.step(dt)
-  p.step()
-  bot.randStep()
-
-def center_image(image):
-  image.anchor_x = image.width // 2
-  image.anchor_y = image.height // 2
-
-window = pyglet.window.Window()
-pyglet.gl.glClearColor(0.7,0.5,0.3, 1)
-
-ball = Ball(pyglet.image.load('ball3.png'), window.width, window.height)
-p = Player(window.height, pyglet.image.load('player.png'))
-bot = Bot(window.height, pyglet.image.load('player.png'), window.width - 20)
-
-pyglet.clock.schedule_interval(mechanic, 1.0/30)
-
-score_label = pyglet.text.Label(text="Player: 0  Bot: 0", x=window.width // 2, y=window.height - 30, anchor_x='center', anchor_y='center')
-
-
-@window.event
-def on_draw():
-  window.clear()
-  score_label.draw()
-  ball.sprite.draw()
-  p.sprite.draw()
-  bot.sprite.draw()
-
-@window.event
-def on_key_press(symbol, mod):
-  if symbol == key.A:
-    print('A')
-  if symbol == key.LEFT:
-    print('left')
-  if symbol == key.UP:
-    p.isMove = direction.up
-  if symbol == key.DOWN:
-    p.isMove = direction.down
-
-@window.event
-def on_key_release(symbol, mod):
-  if symbol == key.UP or symbol == key.DOWN:
-    p.isMove = False
-
-#window.push_handlers(pyglet.window.event.WindowEventLogger())
-
 
 if __name__ == '__main__':
+  g = Game()
   pyglet.app.run()
