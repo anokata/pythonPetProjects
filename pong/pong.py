6dbfe9eadc65ad95dcce24dac6bf07c22eb85a71
anokata <quartenium@gmail.com>
2016-10-03 13:36:54 +0300

    Базово игрок.

--- pong/pong.py
+++ pong/pong.py
@@ -13,25 +13,27 @@ def center_image(image):
 
 window = pyglet.window.Window()
 pyglet.gl.glClearColor(0.7,0.5,0.3, 1)
-img = pyglet.image.load('ball3.png')
-center_image(img)
+
+ball = pyglet.image.load('ball3.png')
+center_image(ball)
+spriteOne = pyglet.sprite.Sprite(ball, x = 20, y = 20)
+
+player = pyglet.image.load('player.png')
+center_image(player)
+spritePlayer = pyglet.sprite.Sprite(player, x = 20, y = window.height // 2)
+playerSpeed = 10
 pyglet.clock.schedule_interval(mechanic, 1.0/30)
 
-spriteOne = pyglet.sprite.Sprite(img, x = 20, y = 20)
-score_label = pyglet.text.Label(text="Player: 0  Bot: 0", x=10, y=window.height - 30)
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+
+score_label = pyglet.text.Label(text="Player: 0  Bot: 0", x=window.width // 2, y=window.height - 30, anchor_x='center', anchor_y='center')
 
 
 @window.event
 def on_draw():
   window.clear()
-  label.draw()
   score_label.draw()
   spriteOne.draw()
+  spritePlayer.draw()
 
 @window.event
 def on_key_press(symbol, mod):
@@ -39,8 +41,10 @@ def on_key_press(symbol, mod):
     print('A')
   if symbol == key.LEFT:
     print('left')
-  #print("pressed", symbol, mod)
-
+  if symbol == key.UP:
+    spritePlayer.y += playerSpeed
+  if symbol == key.DOWN:
+    spritePlayer.y -= playerSpeed
 #window.push_handlers(pyglet.window.event.WindowEventLogger())
 
 
