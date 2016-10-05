8798cdfd21e0c66430a4a9f853edef38afe6bb87
anokata <quartenium@gmail.com>
2016-10-05 21:31:15 +0300

    Задержка перед переходом. Уровни. Вычитание здоровья.

--- pong/pong.py
+++ pong/pong.py
@@ -3,9 +3,10 @@ from pyglet.window import key
 import random
 import math
 #bug: 0
+#ctrlAlt-F F3 F4 CA-t A-o A-w
 def debugDecor(fn):
   """ Декоратор отладки. """
-  def wrap(*args): 
+  def wrap(*args):
     print(args)
     return fn(*args)
   return wrap
@@ -63,7 +64,9 @@ class HasSprite():
     self.sprite.height = h
   width = property(width_get, width_set)
   height = property(height_get, height_set)
-  
+
+  def draw(self):
+    self.sprite.draw()
   def distanceFrom(self, obj):
     return distance(self.x, self.y, obj.x, obj.y)
 #sprite.scale:float
@@ -76,7 +79,7 @@ class Map:
            BlockType.pearl: ('block2anim/block2F', 7),
            BlockType.emerald: ('block3anim/emerald0', 7)}
   levelsNames = ['lv0.lev']  # имена уровней
-  levels = [] # сам уровень формата [(blockType, x, y),...] 
+  levels = [] # сам уровень формата [(blockType, x, y),...]
   levelsCoords = []  # координаты иконок уровней на карте
   levelsCaps = [] # спрайты иконок уровней на карте
   levelsDone = [] # пройденные уровни
@@ -85,7 +88,7 @@ class Map:
   blockWindowLeft = 500
   background = None
   capRadius = 30
-  
+
   def __init__(self, width, height, mapname):
     self.blockWindowTopStart = height
     self.blockWindowLeft = width
@@ -97,7 +100,7 @@ class Map:
     self.loadMap(mapname)
     self.loadBlockImages()
     self.loadLevels()
-    
+
   def loadMap(self, mapname):
     with open(mapname, 'rt') as fin:
       self.background = fin.readline()[:-1] #without \n
@@ -111,7 +114,7 @@ class Map:
         y = int(l[1])
         self.levelsCoords += [(x, y)]
         self.levelsCaps += [pyglet.sprite.Sprite(levelCap, x=x, y=y)]
-      print(self.background, levelDir, self.levelsCoords)
+      #print(self.background, levelDir, self.levelsCoords)
     #load levels list
     self.levelsNames = list()
     from os import listdir
@@ -121,7 +124,7 @@ class Map:
     self.levelsNames.sort()
     #load bg
     self.background = pyglet.sprite.Sprite(pyglet.image.load(self.background))
-    
+
   def loadLevels(self):
     """ Загрузка всех уровней карты. """
     for name in self.levelsNames:
@@ -129,7 +132,7 @@ class Map:
       self.levels += [lev]
       lev.loadBlocks(self)
     self.currentLevel = self.levels[0]
-  
+
   def loadBlockImages(self):
     """ загрузка изображений блоков. """
     for (b, (imgBaseName, framesCount)) in self.blockImgNames.items():
@@ -144,17 +147,16 @@ class Map:
 
   def getBlockAnim(self, btype):
     return self.blocksAnims[btype]
-  
+
   def getDrawables(self):
     return self.currentLevel.getDrawables()
-  
+
   def draw(self):
     self.background.draw()
     for x in self.levelsCaps:
       x.draw()
-  
+
   def loadLevel(self, n):
-    print(n)
     self.currentLevel = self.levels[n]
 
 class Level:
@@ -166,17 +168,17 @@ class Level:
   blockLeft = 500
   blockTop = 5
   blockWindowTopStart = 500
-  
+
   def __init__(self, name, left, top):
     self.blocksGrid = list()
     self.blocks = list()
     self.readLevel(name)
     self.blockLeft = left# - 2.5 * self.blockWidth
     self.blockWindowTopStart = top
-    
+
   def addBlock(self, btype, x, y):
     self.blocksGrid += [(btype, x, y)]
-  
+
   def loadBlocks(self, m):
     """ Загрузка блока. """
     for (b, x, y) in self.blocksGrid:
@@ -184,53 +186,53 @@ class Level:
       x = x * self.blockWidth + self.blockLeft
       y = self.blockWindowTopStart - y * self.blockHeight + self.blockTop
       self.blocks += [BaseBlock(m, x, y, b)]
-    
+
   def writeLevel(self):
     """ Сохранение уровня. """
     with open(self.name, 'wt') as fout:
       for (b, x, y) in self.blocks:
         fout.write(str(b) + ' ' + str(x) + ' ' + str(y) + '\n')
-  
+
   def readLevel(self, name):
     with open(name, 'rt') as fin:
       for line in fin:
         line = line.split()
-        self.addBlock(int(line[0]), int(line[1]), int(line[2]))  # тут координаты сетки  
+        self.addBlock(int(line[0]), int(line[1]), int(line[2]))  # тут координаты сетки
 
   def printLevel(self):
     for (b, x, y) in self.blocksGrid:
       print(b, x, y)
-  
+
   def getDrawables(self):
     return self.blocks
-    
+
   def blockCapture(self, block):
     """ При захвате(уничтожение) блока игроком. """
     self.blocks = list(filter(lambda x: x != block, self.blocks))
-    
+
   def isComplete(self):
     return len(list(filter(lambda x: x.btype == 0, self.blocks))) == 0
-  
+
 class BaseBlock(HasSprite):
   btype = None
   Health = 0
   price = 0
-  
+
   def __init__(self, m, x, y, btype):
     self.sprite = pyglet.sprite.Sprite(m.getBlockAnim(btype), x=x, y=y)
     self.btype = btype
     # stats(Heath, price)
-    baseStats = {BlockType.emerald: (2, 30), 
+    baseStats = {BlockType.emerald: (2, 30),
       BlockType.ruby: (3, 150),
       BlockType.pearl: (1, 10)}
     (self.Health, self.price) = baseStats[btype]
-    
+
   def capture(self):
     self.sprite.visible = False
-  
+
   def draw(self):
     self.sprite.draw()
-    
+
 # make fabric?
 class ImgNumber:
   """ Генератор цифр числа из изображений. С анимацией. """
@@ -251,31 +253,31 @@ class ImgNumber:
       if self.lifeTime != -1:
         self.fadeout()
       return self.lifeTime
-      
+
     def fadeout(self):
       """ Исчезание. """
       spd = 8
       for x in self.number:
         if x.opacity - spd >= 0:
           x.opacity -= spd
-  
+
   images = []
   numbers = []
-  
+
   def __init__(self):
     self.images = list()
     self.numbers = list()
     for i in range(10):
       self.images += [pyglet.image.load(str(i) + '.png')]
-  
+
   def draw(self):
     for i in self.numbers:
       i.draw()
-  
+
   def step(self):
     self.numbers[:] = filter(lambda x: x.step() != 0, self.numbers)
-  
-  def makeNumber(self, n, x, y, lifeTime=-1): 
+
+  def makeNumber(self, n, x, y, lifeTime=-1):
     """ Фабрика создающая число. """
     n = str(n)
     number = list()
@@ -286,7 +288,7 @@ class ImgNumber:
       x += num.width
       number += [num]
     self.numbers += [self.DrawableNumber(number, lifeTime)]
-      
+
 class Particles:
   """ Система частиц. """
   sprites = []
@@ -294,7 +296,7 @@ class Particles:
   lifeTime = 100
   initLifeTime = 0
   speedDown = 0
-  
+
   def __init__(self, img, n, lifeTime, x=0, y=0, speed=2, speedDown=0):
     self.sprites = list()  # Необходимо все неэлемнтарные типы так создавать
     self.lifeTime = 0
@@ -306,12 +308,12 @@ class Particles:
       s.dx = math.cos(math.radians(360*i/n)) * self.speed
       s.dy = math.sin(math.radians(360*i/n)) * self.speed
       self.sprites += [s]
-  
+
   def draw(self):
     if 0 != self.lifeTime:
       for x in self.sprites:
-        x.draw() 
-  
+        x.draw()
+
   def step(self):
     if 0 == self.lifeTime:
       self.end()
@@ -323,10 +325,10 @@ class Particles:
       x.dx *= self.speedDown
       x.dy *= self.speedDown
       x.opacity -= 30
-  
+
   def end(self):
     pass
-  
+
   def restart(self, x, y):
     for i in self.sprites:
       i.x = x
@@ -340,18 +342,18 @@ class Game:
   ball = None
   player = None
   bot = None
-  
+
   window = None
   labels = []
   labelsFun = []
   difficult = 100
-  
+
   gameWindowLeft = 20
   gameWindowUp = 20
   gameWindowWidth = 0
   gameWindowHeigth = 0
   enemyDeep = 80
-  
+
   wall = None
   redParticles = None
   blueParticles = None
@@ -359,41 +361,41 @@ class Game:
   drawable = []
   stepping = []
   levelDrawable = []
-  
+
   state = 1
   stateRun = 1
   stateBallCapt = 2
   stateMap = 3
   blockAreaLeft = 0
-  
+
   background = None
   foreground = None
   mmap = None
   mmap = None
-  
+
   def __init__(self):
     random.seed()
     self.drawable = list()
     self.stepping = list()
     self.labels = list()
     self.labelsFun = list()
-    
+
     self.window = pyglet.window.Window(width=800, height = 550)
     window = self.window
     self.gameWindowHeigth = window.height - 20
     self.gameWindowWidth = window.width - 200
     self.blockAreaLeft = self.gameWindowWidth - 2.5 * Level.blockWidth
-    
+
     self.background = pyglet.sprite.Sprite(pyglet.image.load('board1.png'), x=self.gameWindowLeft)
     self.addToDrawable(self.background)
     self.foreground = pyglet.sprite.Sprite(pyglet.image.load('foreground.png'))
     self.addToDrawable(self.foreground)
-    
+
     self.ball = Ball(pyglet.image.load('ball4.png'), self.gameWindowWidth, self.gameWindowHeigth, self.gameWindowLeft)
-    
+
     self.player = Player(pyglet.image.load('player.png'), self.gameWindowLeft, self.gameWindowLeft , self.gameWindowHeigth - self.gameWindowLeft)
     self.bot = Bot(pyglet.image.load('player.png'), self.gameWindowWidth - Level.blockWidth * 3, self.gameWindowLeft, self.gameWindowHeigth - self.gameWindowLeft)
-    
+
     pyglet.gl.glClearColor(0.7,0.5,0.3, 1)
     #window.push_handlers(pyglet.window.event.WindowEventLogger())
     pyglet.clock.schedule_interval(self.stateHandle, 1.0/30)
@@ -404,14 +406,14 @@ class Game:
     self.addParticles(self.redParticles)
     self.addParticles(self.blueParticles)
     self.addParticles(self.whiteParticles)
-    
+
     self.numberGenerator = ImgNumber()
     self.addToDrawable(self.numberGenerator)
     self.addToStepping(self.numberGenerator)
-    
+
     self.mmap = Map(self.blockAreaLeft, self.gameWindowHeigth - 25, 'map1.map')
     self.setLevelDrawable(self.mmap.getDrawables())
-    
+
     self.stateToBallCapt()
 
     self.addLabel(self.player.getStatText , 'Health: 0', PlayerStatIndex.Health)
@@ -421,7 +423,7 @@ class Game:
     self.addLabel(self.player.getStatText , '', PlayerStatIndex.statPoints)
     self.addLabel(self.player.getStatText , '', PlayerStatIndex.Speed)
     self.updateLabels()
-    
+
     @window.event
     def on_draw():
       self.stateHandleDraw()
@@ -434,19 +436,19 @@ class Game:
     @window.event
     def on_mouse_press(x, y, button, mod):
       self.stateHanderMousePress(x, y, button, mod)
-  
+
   def addLabel(self, updateFun, initText, updateParam):
     self.labels += [pyglet.text.Label(text=initText, x=self.window.width - 90, y=self.gameWindowHeigth - 20 * len(self.labels) - 20)]
     self.labelsFun += [(updateFun, updateParam)]
-    
+
   def setLevelDrawable(self, x):
     """ Установка отрисовывающегося на уровне. """
     self.levelDrawable = x
-  
+
   def addParticles(self, x):
     self.addToDrawable(x)
     self.addToStepping(x)
-    
+
   def addToDrawable(self, x):
     self.drawable += [x]
   def addToStepping(self, x):
@@ -455,21 +457,29 @@ class Game:
   def stateSet(self, s):
     """ Установка состояния. """
     self.state = s
-  
+
   def stateToRun(self):
     """ Переход в состояние игры полёта мяча. """
     if self.state == self.stateBallCapt:
       self.ball.drop(self.player.speed * 10 * self.player.isMove)
     self.stateSet(self.stateRun)
-    
+
   def stateToBallCapt(self):
     """ К захвату мяча. """
     self.ball.x = self.player.x + self.player.width
     self.stateSet(self.stateBallCapt)
-  
-  def stateToMap(self):
+
+  def stateToMap(self, dt):
     """ Переход на карту. """
+    cap = self.mmap.levelsCaps[self.mmap.levels.index(self.mmap.currentLevel)]
+    self.ball.x = cap.x
+    self.ball.y = cap.y
     self.stateSet(self.stateMap)
+
+  def stateIntervalToMap(self):
+    #from pyglet import clock
+    pyglet.clock.schedule_once(self.stateToMap, 2)
+
   #Обработчики механики от состояния
   def stateHandle(self, dt):
     d = {
@@ -497,7 +507,7 @@ class Game:
     }
     fun = d[self.state]
     fun(symbol, mod)
-  
+
   def stateHandleKeyRelease(self, symbol, mod):
     d = {
       self.stateBallCapt: self.playKeyRelease,
@@ -506,7 +516,7 @@ class Game:
     }
     fun = d[self.state]
     fun(symbol, mod)
-    
+
   def stateHanderMousePress(self, x, y, button, mod):
     d = {
       self.stateBallCapt: False,
@@ -528,19 +538,20 @@ class Game:
     self.bot.sprite.draw()
     for x in self.labels:
       x.draw()
-    
+
   def mapDraw(self):
     self.window.clear()
     self.mmap.draw()
-    
+    self.ball.draw()
+
   # Key events
   def mapKeyPress(self, symbol, mod):
     if symbol == key.ENTER:
       self.stateToBallCapt()
-  
+
   def mapKeyRelease(self, symbol, mod):
     pass
-    
+
   def playKeyPress(self, symbol, mod):
     if symbol == key.RIGHT:
       g.player.isMove = direction.down
@@ -560,7 +571,7 @@ class Game:
       g.player.isMove = False
 
   def mapMousePress(self, x, y, b, m):
-    """ Проверяем попал ли игрок на иконку уровня. 
+    """ Проверяем попал ли игрок на иконку уровня.
         И если да, то загружаем и запускаем его.
     """
     print(x, y)
@@ -568,14 +579,14 @@ class Game:
       if distance(x, y, a, b) < self.mmap.capRadius:
         self.mmap.loadLevel(self.mmap.levelsCoords.index((a,b)))
         self.stateToBallCapt()
-        
+
         self.setLevelDrawable(self.mmap.getDrawables())
         return
-    
+
   # Mechanic
   def mechanicMap(self, dt):
     pass
-  
+
   def mechanicStd(self, dt):
     """ Общая механика игры. """
     self.updateLabels()
@@ -585,7 +596,7 @@ class Game:
       x.step()
     if self.mmap.currentLevel.isComplete():
       self.stateToMap()
-    
+
   def mechanicCapt(self, dt):
     """ Механика при захваченном мяче. """
     self.mechanicStd(dt)
@@ -601,7 +612,7 @@ class Game:
     self.isCollision()
     self.blockCollision()
     self.mechanicStd(dt)
-    
+
   # Physics
   def blockCollision(self):
     """ Пересечения с блоками мяча. """
@@ -621,10 +632,10 @@ class Game:
             self.numberGenerator.makeNumber(b.price, b.x, b.y, 30)
             self.mmap.currentLevel.blockCapture(b)
             self.updateLabels()
-    
+
   def ballCaptureStep(self):
     self.ball.y = self.player.y
-    
+
   def isCollision(self):
     """ Обработка пересечения игроков с мячом. """
     colis = self.collisoinsDetect()
@@ -636,7 +647,7 @@ class Game:
         self.whiteParticles.restart(self.ball.left, colis.y)
       else:
         self.ball.dx = - abs(self.ball.dx)
-      
+
       if colis.isMove:
         self.ball.dy += colis.isMove * colis.speed * 2
   # 2down Refactor
@@ -647,7 +658,7 @@ class Game:
     if self.ball.x > self.gameWindowWidth + self.enemyDeep:
       return 1
     return 0
-    
+
   def checkBallOut(self):
     isout = self.isBallOut()
     if 1 == isout:
@@ -655,14 +666,16 @@ class Game:
       self.ball.bounce()
     if -1 == isout:
       self.lose()
-  
+
   def lose(self):
     """ Проигрыш при неотбивании. """
     self.updateLabels()
     self.redParticles.restart(self.ball.x, self.ball.y)
     self.stateToBallCapt()
-    #TODO: health down
-  
+    self.player.Health -= 2
+    if self.player.Health < 1:
+      self.stateIntervalToMap()
+
   def collisoinsDetect(self):
     """ Проверка на пересечения игроков с мячом. """
     faces = [self.player, self.bot]
@@ -671,15 +684,15 @@ class Game:
       allheight = self.ball.height + x.height
       wid = max(self.ball.left, x.left) - min(self.ball.right, x.right)
       allwid = self.ball.width + 0
-      
+
       if hei <= allheight and wid <= allwid:
         return x
     return False
-  
+
   def updateLabels(self):
     for (fun, param), lab in zip(self.labelsFun, self.labels):
       lab.text = fun(param)
-    
+
 
 class direction:
   """ Направления движения игрока\бота. """
@@ -710,12 +723,12 @@ class Player(HasSprite):
   Exp = 0
   Lv = 0
   statPoints = 0
-  
+
   LevelExp = {
     1: 100, 2: 300, 3: 1000 # есть генератор
   }
-  
-  
+
+
   #@debugDecor
   def __init__(self, img, x, yMin, yMax):
     center_image(img)
@@ -725,11 +738,11 @@ class Player(HasSprite):
     self.sprite = pyglet.sprite.Sprite(img, x = x, y = yMin)
     self.yMin = yMin
     self.yMax = yMax
-    self.left = x 
-    self.right = x 
+    self.left = x
+    self.right = x
     self.LevelExp = dict(enumerate(geomRange(10, 51, 1.2)))
     #print(self.LevelExp)
-  
+
   def step(self):
     if self.isMove == direction.up:
       if (self.y ) < self.yMax:
@@ -737,15 +750,15 @@ class Player(HasSprite):
     if self.isMove == direction.down:
       if (self.y ) > self.yMin:
         self.y -= self.speed
-    
+
     self.top = self.y + self.height // 2
     self.bottom = self.y - self.height // 2
-    
+
   def capture(self, block):
     """ Получение блока и опыта. """
     self.expGain(block.price)
     block.capture()
-  
+
   def expGain(self, n):
     """ Получение опыта. """
     # смотрим сколько до след уровня
@@ -757,7 +770,7 @@ class Player(HasSprite):
     self.Lv += 1
     self.statPoints += 1
     self.expGain(n - tonext)
-    
+
   def getStatText(self, stat):
     """ Получение описания и значения стата. """
     stats = {
@@ -782,7 +795,7 @@ class Bot(Player):
     self.left = x + img.width
     self.right = x
     self.speed = 1 # difficult
-  
+
   def randStep(self, ballY, inBotArea):
     """ Поведение бота. """
     self.i += 1
@@ -817,7 +830,7 @@ class Ball(HasSprite):
   top = 0
   bottom = 0
   maxspeed = 1000
-  
+
   def __init__(self, img, width, height, marginBottom):
     self.collided = list()
     center_image(img)
@@ -827,7 +840,7 @@ class Ball(HasSprite):
     self.wh = img.width // 2
     self.yMax = height - self.wh
     self.xMax = width - self.wh
-    self.yMin = img.height // 2 + marginBottom 
+    self.yMin = img.height // 2 + marginBottom
 
   def step(self, dt):
     self.sprite.rotation += 100 * dt
@@ -836,20 +849,20 @@ class Ball(HasSprite):
     self.lastdt = dt
     self.recalcCoords()
     self.collisoins()
-  
+
   def recalcCoords(self):
     """ Пересчёт крайних координат. """
     self.right = self.x + self.width // 2
     self.left = self.x - self.width // 2
     self.top = self.y + self.height // 2
     self.bottom = self.y - self.height // 2
-  
+
   def stepBack(self):
     """ Шаг назад. """
     self.x -= self.dx * self.lastdt
     self.y -= self.dy * self.lastdt
     self.recalcCoords()
-  
+
   def bounce(self):
     self.stepBack()
     self.dx = -self.dx
@@ -860,19 +873,18 @@ class Ball(HasSprite):
       self.dy = - self.dy
     if self.y > self.yMax:
       self.dy = - self.dy
-  
+
   def ballReturn(self, direct):
     """ Возврат. """
     self.x = self.midX
     self.y = self.midY
     self.dx = self.speed * direct
     self.dy = 200
-  
+
   def drop(self, dy):
     """ Выброс мяча от игрока. """
     self.dy = dy if dy != 0 else random.randint(-50,50)
     self.dx = self.speed
-
 
 if __name__ == '__main__':
   g = Game()
