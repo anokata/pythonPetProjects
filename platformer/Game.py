d745f3d547a796a1dedd3e26bae889709a28ee32
anokata <quartenium@gmail.com>
2016-10-06 11:56:49 +0300

    Изучаем, хеллоувордим.

---
+++ platformer/Game.py
@@ -0,0 +1,20 @@
+""" Main class module. """
+import cocos
+
+class Game(cocos.layer.Layer):
+    """ Main class """
+
+    def __init__(self):
+        """ Init."""
+        super(Game, self).__init__()
+
+    def run(self):
+        """ Run the game. IO -> IO"""
+        pass
+
+def test():
+    """ Test module fun. """
+    pass
+
+if __name__ == "__main__":
+    test()
--- platformer/main.py
+++ platformer/main.py
@@ -1,8 +1,12 @@
 """ Main module. """
+import cocos
+import Game
 
 def main():
     """ Main function. """
-    pass
+    cocos.director.director.init()
+    cocos.director.director.run(cocos.scene.Scene(Game.Game()))
+    #game.run()
 
 if __name__ == "__main__":
     # в каждом другом модуле будет так же проверка и запуск тестов.(в модуле теста для этого файла)
