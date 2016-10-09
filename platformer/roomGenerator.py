22b05599fc04ce47ee2324cb6ba5617bc7751f0c
anokata <quartenium@gmail.com>
2016-10-09 15:56:17 +0300

    Продолжаю разрабатывать лабиринт генератор.

--- platformer/roomGenerator.py
+++ platformer/roomGenerator.py
@@ -1,3 +1,33 @@
+def makeHole(r, x, y):
+    return list(filter(lambda a: a != (x, y), r))
+
+def makeHWall(x, y, w):
+    r = list()
+    for i in range(x, x + w + 1):
+        r.append((i, y))
+    return r
+
+def makeTunnelH(r, sx, y, ex):
+    r = makeHole(r, sx, y)
+    r = makeHole(r, ex, y)
+    r += (makeHWall(sx, y+1, ex-sx))
+    r += (makeHWall(sx, y-1, ex-sx))
+    return r
+
+
+def makeRoomWithHoles(x, y, w, h):
+    r = makeRoom(x, y, w, h)
+    l = (w + h) // 4
+    if w > l:
+        #r = list(filter(lambda a: a != (l+x, y) and a != (l+x, y+h), r))
+        r = makeHole(r, l+x, y)
+        r = makeHole(r, l+x, y+h)
+
+    if h > l:
+        r = list(filter(lambda a: a != (x, y+l) and a != (x+w, y+l), r))
+
+    return r
+
 def makeRoom(x, y, w, h):
     r = [(x, y)]
     for i in range(x + 1, x + w + 1):
@@ -34,13 +64,18 @@ def lab2StrLst(l):
         r = setXY(r, x, y, w+1, '#')
     return r
 
+def makeSuccsessiveRooms(n):
+    pass
+
 if __name__ == '__main__':
     # test
     r = makeRoom(2,8, 4, 4)
-    print(r)
     r += makeRoom(0,0, 3, 6)
-    r += makeRoom(9,1, 5, 5)
-    r += makeRoom(9,8, 5, 5)
+    r += makeRoomWithHoles(9,1, 5, 5)
+    r += makeRoomWithHoles(9,8, 5, 5)
     r += makeRoom(9,15, 5, 5)
+    print(r)
+    r = makeTunnelH(r, 6, 10, 9)
+    print(r)
     print(lab2StrLst(r))
     print(setXY('aaaaaaaaa', 1, 1, 3, 'z'))
