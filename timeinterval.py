e9944f43766587d224f014f5fc4c7651d04e069c
anokata <quartenium@gmail.com>
2016-10-19 12:57:59 +0300

    доделал напоминалку.

--- timeinterval.py
+++ timeinterval.py
@@ -7,11 +7,13 @@ exc = subprocess.getoutput
 arg = sys.argv
 
 if len(arg) == 1 or not arg[1].isnumeric():
-    print("Необходимо ввести агрумент - количество минут")
+    print("Необходимо ввести агрумент - количество минут, и, возможно, сообщение.")
     exit()
 minutes = int(sys.argv[1])
 print("Я сообщу через: %d минут." % minutes)
 time.sleep(minutes * 60)
 msg = "Прошло %d минут!" % minutes
+if len(arg) > 2:
+    msg += '\n' + arg[2]
 print(msg)
-exc('zenity --info --text=' + msg)
+exc('zenity --info --text="' + msg + '"')
