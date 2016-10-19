ad6f4dce7c7f7ed3ed1dddc0112416b15859078f
anokata <quartenium@gmail.com>
2016-10-19 10:59:05 +0300

    Получаю данные погоды.

--- weather/weather.py
+++ weather/weather.py
@@ -1,3 +1,14 @@
-# Берём данные xml. 
+# выбираем узлы time from для текущего дня.
+# из данных temperature value вычисляем среднее
+
+import subprocess
+exc = subprocess.getoutput
+# получаем текущую дату - день.
+day = int(exc('date +%d'))
+
+import requests as req
 url = 'http://www.yr.no/place/Russia/Yaroslavl/Rybinsk/forecast_hour_by_hour.xml'
+# Берём данные xml.  weatherdata.forecast.tabular
+req.get(url)
+frc = req.xml()
 
