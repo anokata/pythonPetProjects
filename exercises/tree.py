427f7ffd9cfe262e6767b9b5142181bc0ab62335
anokata <quartenium@gmail.com>
2016-10-21 10:50:14 +0300

    Двоичные деревья на словаре. Красивый словарь. код хаффмана начало.

--- exercises/tree.py
+++ exercises/tree.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Tree:
     """Реализация дерева на списках"""
     # если хранить текущий узел и возвращать self то можно делать типа tree.left.left.right?
@@ -27,8 +28,62 @@ class Tree:
         for n in self.children:
             t += '\n' + (' ' * (self.deep-1)*2) + '|-'  + n.show()
         return t
-        
 
+class pddict(defaultdict):
+    def __str__(self):
+        t =''
+        for k, v in self.items():
+            t += str(k) + ': ' + str(v) + '\n'
+        return t
+class pdict(dict):
+    def __str__(self):
+        t =''
+        for k, v in self.items():
+            t += str(k) + ': ' + str(v) + '\n'
+        return t
+        
+class BTree(pdict):
+    val = ''
+
+    def __init__(self):
+        super().__init__(self)
+        self['left'] = ''
+        self['right'] = ''
+    
+    def _left(self):
+        return self['left']
+
+    def _right(self):
+        return self['right']
+    
+    def _sleft(self, val):
+        b = BTree()
+        b.val = val
+        self['left'] = b
+        return self._left
+
+    def _sright(self, val):
+        b = BTree()
+        b.val = val
+        self['right'] = b
+        return self._right
+
+    left = property(_left, _sleft)
+    right = property(_right, _sright)
+
+    def __repr__(self):
+        t = '{' + str(self.val)
+        t += '\n'
+        t += str(self.left)
+        t += '\n'
+        t += str(self.right)
+        t += '}'
+        return t
+    __str__ = __repr__
+
+
+# сделать двоичное дерево на словарях
+# сделать алгоритм хаффмана
 if __name__ == '__main__':
     t = Tree('a')
     t.add('b')
@@ -39,5 +94,21 @@ if __name__ == '__main__':
     n.add('z3')
     print(t)
     print(t.show())
+    msg = 'aadlfkfjafjsdlfjsdlfjaskldfjalfjlkcoixucvxucvuxuvuxuvxocvicxoaaoiuadaaaaaoifafu'
+    # посчитаем количество каждого символа
+    freq = pddict(int)
+    for x in msg:
+        freq[x] += 1
+    # вычислим вероятность появления каждого символа
+    for k, v in freq.items():
+        freq[k] = (v, v/len(msg))
+    print(freq)
+
+
+    b = BTree()
+    b.val = 10
+    b.left = 1
+    print(b)
+
 
 
