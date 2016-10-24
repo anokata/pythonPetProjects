49640ccd41454730dedece9d5c1d90f077aac69d
anokata <quartenium@gmail.com>
2016-10-24 14:03:05 +0300

    добавил поддиректории, делаю...

--- zametki/vault.py
+++ zametki/vault.py
@@ -4,51 +4,137 @@ import datetime
 import os
 
 class pdict(dict):
-    def __str__(self):
+    def __str__(self, pfx=''):
         t =''
         for k, v in self.items():
-            t += str(k) + ': ' + str(v) + '\n'
+            t += pfx + str(k) + ': ' + str(v) + '\n'
         return t
-      
-class Storage():
-    storage = pdict()
-    fn = 'storage.dat'
+class StorageItem():
 
-    def __init__(self):
-        if os.path.exists(self.fn) and os.path.isfile(self.fn):
-            self.load()
+    def __init__(self, nowstr, val):
+        self.val = (nowstr, val)
+
+    def __str__(self, pfx=''):
+        return pfx + str(self.val)
+
+    def __getitem__(self, k):
+        return self.val[1][k]
+    def get(self):
+        return self.val
+    def __setitem__(self, k, v):
+        self.val[1][k] = v
+
+    def addDir(self, x):
+        self.val[1].addDir(x)
+
+    def isDir(self):
+        return False
+
+class StorageDir():
+    storage = pdict() #!!!
+
+    def __init__(self, name='main'):
+        self.storage = pdict()
+        self.name = name
+        self.nowstr = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
 
     def __setitem__(self, key, val):
         nowstr = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-        val = (nowstr, base64.b64encode(bytes(val, 'utf-8')))
+        if type(val) != StorageDir:
+            val = StorageItem(nowstr, base64.b64encode(bytes(val, 'utf-8')))
+        else:
+            val = StorageItem(nowstr, val)
         self.storage[key] = val
 
+    def getItems(self):
+        return self.storage
+
     def __getitem__(self, key):
-        now, val = self.storage[key]
-        val = base64.b64decode(val).decode('utf-8')
-        return (now, val)
+        val = self.storage[key]
+        now = 'x'
+        if type(val) != StorageDir:
+            now, val = val.get()
+            val = base64.b64decode(val).decode('utf-8')
+        else:
+            now = val.nowstr
+        return StorageItem(now, val)
+
+    def __str__(self, pfx=''):
+        return pfx + str(self.storage)
+
+    def addDir(self, name):
+        self.storage[name] = StorageDir(name)
+
+    def get(self):
+        return ('', self.name)
+
+    def isDir(self):
+        return True
+
+    def pprint(self):
+        for k, v in self.storage.items():
+            print(' ', k, '||', v, type(v))
+
+class Storage():
+    fn = 'storage.dat'
+    mainDir = 0
+
+    def __init__(self, isLoad=True):
+        if os.path.exists(self.fn) and os.path.isfile(self.fn) and isLoad:
+            self.load()
+        else:
+            self.mainDir = StorageDir()
+
+    def __setitem__(self, k, v):
+        self.mainDir.__setitem__(k, v)
+
+    def __getitem__(self, k):
+        return self.mainDir[k]
 
     def load(self):
         fl = open(self.fn, 'rb')
-        self.storage = pickle.load(fl)
+        self.mainDir = pickle.load(fl)
         fl.close()
 
     def save(self):
         fl = open(self.fn, 'wb')
-        pickle.dump(self.storage, fl)
+        pickle.dump(self.mainDir, fl)
         fl.close()
 
     def __str__(self):
-        return str(self.storage)
+        return '' + str(self.mainDir)
+
  
+    def addDir(self, name):
+        self.mainDir.addDir(name)
+
+    def pprint(self):
+        self.mainDir.pprint()
+
  
 if __name__ == '__main__':
-    #test
-    storage = Storage()
-    a = storage['etst']
-    print(a)
-    #storage['etst2'] = 'abc'
+    # types text url urllist dir(as substorage)
+    storage = Storage(False)
+    storage['mainValue'] = '10'
+    storage.addDir('Dir1')
+    storage.addDir('Dir2')
+    storage['Dir1']['subdirval'] = '123876.23'
+    storage['Dir2']['subdirval'] = '1ll.23'
+    storage['Dir2'].addDir('SubDir0')
+    storage['Dir2']['SubDir0']['1'] = 'TYY'
     storage.save()
-    print(storage)
+    storage2 = Storage(True)
+    print(storage2)
+    print('*'*8)
+    print(storage2['mainValue'])
+    #print(storage2['subdirval'])
+    print(storage2['Dir1']['subdirval'])
+    print(storage2['Dir1'])
+    print(storage2.mainDir.storage.keys())
+    print(storage2['Dir2']['SubDir0']['1'])
+    print('*'*8)
+    storage2.pprint()
+    print('*'*8)
+    print(storage2['Dir1'].storage.keys())
 
 
