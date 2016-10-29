0eb33255555512c3bf45e3006e2747ed1ea8ceec
anokata <quartenium@gmail.com>
2016-10-29 13:21:45 +0300

    Добавил нормальные опции

---
+++ wallcom.sh
@@ -0,0 +1,1 @@
+python3 wallpaperText.py --file miniTasks.txt --bg "/home/ksi/Downloads/fontain.JPG" --fontsize 25
--- wallpaperText.py
+++ wallpaperText.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
 import subprocess
 exc = subprocess.getoutput
-from sys import argv
+import optparse
 from PIL import Image, ImageDraw, ImageFont
 # прога для рисования на изображении текста(из файла, статус системы...) и отображения его на фон, периодически
-# args = periodic(secs?) textfile/syscmd to exec, bgimg file, fontcolor
+# args = periodic(secs?) textfile/syscmd to exec, bgimg file, fontcolor, fontsize
+# ARGV names изучить как там их обрабатывать
 # args img, 
 W = 1280
 H = 1024
-fn = 'wallpaperText.py'
+p = optparse.OptionParser()
+p.add_option('--bg', default="")
+p.add_option('--file', default="")
+p.add_option('--fontsize', default=20)
+options, arguments = p.parse_args()
+
+if options.bg != '':
+    imgpath = options.bg
+    txt = Image.open(imgpath)
+else:
+    txt = Image.new('RGB', (W, H), (0,0,0))
+
+if options.file != '':
+    fn = options.file
+else:
+    fn = 'wallpaperText.py'
 # создать имг или загрузить фон.
 fontPath = "/usr/share/fonts/truetype/dejavu/"
 f1 = 'DejaVuSansMono.ttf'
 f2 = "DejaVuSans.ttf"
 fontPath += f1
 #fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 20)
-fnt = ImageFont.truetype(fontPath, 20)
+fnt = ImageFont.truetype(fontPath, int(options.fontsize))
 # нанести текст(файла)
-if len(argv)>1:
-    imgpath = argv[1]
-    txt = Image.open(imgpath)
-else:
-    txt = Image.new('RGB', (W, H), (0,0,0))
 #print(txt, fnt)
 d = ImageDraw.Draw(txt)
 #d.text((10,10), "Hello", font=fnt, fill=(255,255,255))
@@ -32,6 +43,6 @@ d.multiline_text((20,10), text, fill=None, font=fnt, anchor=None, spacing=5, ali
 #txt.show()
 # сохранить в файл
 imgfn = 'walltext.png' 
-txt.save(imgfn, 'PNG', optimize=True)
+txt.save(imgfn, 'PNG', optimize=True, compress_level=6)
 # сделать фоном
 exc('qiv -z ' + imgfn)
