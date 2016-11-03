7b9be1d348b0ef497422c452aa906f32fdecffc8
anokata <quartenium@gmail.com>
2016-11-03 17:44:16 +0300

    euler

--- exercises/euler41.py
+++ exercises/euler41.py
@@ -1,15 +1,31 @@
-import primes
 from itertools import permutations
-n = 7
-l = ''.join([chr(x+49) for x in range(n)])
+n = 10
+l = ''.join([chr(x+48) for x in range(n)])
 print(l)
-all = permutations(l) # max  = 900 050 021
-#primes.genprimes(1000000)
+all = permutations(l) 
 nums = list()
+sm = 0
 for x in all:
-    if x[n-1] not in '2468':
-        nums.append(int(''.join(x)))
-print(nums[:10], len(nums))
-nums = list(filter(primes.justPrime, nums))
-print(nums[:10], len(nums))
-print(max(nums))
+    if x[0] != '0':
+        n = ''.join(x)
+        d2d4 = int(''.join(x[1:4]))
+        if d2d4 % 2 != 0:
+            continue
+        
+        if int(''.join(x[2:5])) % 3 != 0:
+            continue
+        if int(''.join(x[3:6])) % 5 != 0:
+            continue
+        if int(''.join(x[4:7])) % 7 != 0:
+            continue
+        if int(''.join(x[5:8])) % 11 != 0:
+            continue
+        if int(''.join(x[6:9])) % 13 != 0:
+            continue
+        if int(''.join(x[7:10])) % 17 != 0:
+            continue
+
+        #nums.append(int(n))
+        sm += int(n)
+#print(nums[:10], len(nums))
+print(sm)
