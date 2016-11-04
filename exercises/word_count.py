66200ecc1053ce00421a79f62867e93695f9600f
anokata <quartenium@gmail.com>
2016-11-04 11:01:27 +0300

    тесты и задание

--- exercises/word_count.py
+++ exercises/word_count.py
@@ -27,12 +27,20 @@ def word_count(s):
 
 def word_count_func(s):
     def wd(s, char, in_word, words):
+        print('fun>', s, "'", char,"'", ord(char),  in_word, words)
         if s == '' and in_word:
             return words + 1
         if s == '':
             return words
-        if (in_word and char != ' ') or (!in_word and char == ' '):
-            wd(s[1:], s[0], 
+        if (in_word and char != ' ') :
+            return wd(s[1:], s[0], in_word, words )
+        if (in_word and char == ' '):
+            return wd(s[1:], s[0], False, words + 1)
+        if (not in_word and char != ' '):
+            return wd(s[1:], s[0], True, words)
+        if (not in_word and char == ' '):
+            return wd(s[1:], s[0], in_word, words)
+    return wd(s, ' ', False, 0)
 
 class Test_word_count(unittest.TestCase):
     def test(self):
@@ -41,6 +49,11 @@ class Test_word_count(unittest.TestCase):
         self.assertEqual(word_count('one'), 1)
         self.assertEqual(word_count('one, two three'), 3)
         self.assertEqual(word_count(' one, two three# ehu&, '), 4)
+        self.assertEqual(word_count_func(''), 0)
+        self.assertEqual(word_count_func('   '), 0)
+        self.assertEqual(word_count_func('one'), 1)
+        self.assertEqual(word_count_func('one, two three'), 3)
+        self.assertEqual(word_count_func(' one, two three# ehu&, '), 4)
 
 if __name__=="__main__":
     unittest.main()
