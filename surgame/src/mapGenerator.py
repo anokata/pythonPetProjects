65a18a5bf70a7d88b94836770998592042fb4601
anokata <quartenium@gmail.com>
2016-11-16 15:01:13 +0300

    Переделал генератор для создания YAML карты.

--- surgame/src/mapGenerator.py
+++ surgame/src/mapGenerator.py
@@ -1,32 +1,34 @@
 import caveGenerate
+import yaml
 
 filename = 'data/cavegen.map'
 
 def addObjects():
-    o = ''
-    o += 'floor\n'
-    o += 'ground\n'
+    o = list()
+    o.append('floor')
+    o.append('ground')
     return o
 
 def writeMap(fn, m):
+    data = yaml.dump(m, default_flow_style=False)
     with open(fn, 'wt') as fout:
-        fout.write(m)
+        print(data)
+        fout.write(data)
 
 def genMap():
-    m = ''
-    map_str = caveGenerate.drawString(caveGenerate.m)
-    map_floor = caveGenerate.drawString(caveGenerate.floor)
-    m += '2\n'
-    m += map_floor
-    m += '\n'
-    m += map_str
-    m += '\n'
-    m += addObjects()
-    m += 'endObjectNames'
-    m += '\n'
+    m = dict()
+    genmap = caveGenerate.gen(50, 5)
+    floor = caveGenerate.init_matrix(50, caveGenerate.FLOOR)
+    map_str = caveGenerate.drawString(genmap)
+    map_floor = caveGenerate.drawString(floor)
+    m['layers_count'] = 2
+    m['layers'] = list()
+    m['layers'].append(map_floor)
+    m['layers'].append(map_str)
+    m['objects'] = addObjects()
     return m
 
 if __name__ == '__main__':
     m = genMap()
     print(m)
-    writeMap('../data/cavegen.map', m) 
+    writeMap('../data/cavegen.yaml', m) 
