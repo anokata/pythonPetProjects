9dbf9e3dfd9d979809885e84866802f663effbc1
anokata <anokata8@mail.ru>
2016-11-18 18:01:14 +0300

    impl Mutable Named Obj

--- exercises/euler59.py
+++ exercises/euler59.py
@@ -1,26 +1,42 @@
-data = 0
-with open('/home/ksi/p059_cipher.txt', 'rt') as fin:
-    data = '[' +  fin.read() + ']'
-    data = eval(data)
+class MutableNamedTuple():
+    def __init__(self, **kwargs):
+        self._keys = list()
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+            self._keys.append(k)
+    def __str__(self):
+        s = ''
+        for k in self._keys:
+            s += str(k) + ':' + str(getattr(self, k)) + '\n'
+        return s
 
-def code_to_str(a):
-    return ''.join(map(chr, a))
+a = MutableNamedTuple(c=213, bb='a')
+print(a)
 
-def meaning(s):
-    if s.find('\0') > 0:
-        return False
-    return s.find('the') > 0
+def euler59():
+    data = 0
+    with open('/home/ksi/p059_cipher.txt', 'rt') as fin:
+        data = '[' +  fin.read() + ']'
+        data = eval(data)
 
-def dexor(s, key):
-    return list(map(lambda x: x ^ key, s))
+    def code_to_str(a):
+        return ''.join(map(chr, a))
 
-key = 0
-s = code_to_str(data)
-while not meaning(s) and key <= 255:
-    key += 1
+    def meaning(s):
+        if s.find('\0') > 0:
+            return False
+        return s.find('the') > 0
+
+    def dexor(s, key):
+        return list(map(lambda x: x ^ key, s))
+
+    key = 0
+    s = code_to_str(data)
+    while not meaning(s) and key <= 255:
+        key += 1
+        s = code_to_str(dexor(data, key))
+        print(key, s[10:20])
+    print(key)
     s = code_to_str(dexor(data, key))
-    print(key, s[10:20])
-print(key)
-s = code_to_str(dexor(data, key))
-print(s)
+    print(s)
 
