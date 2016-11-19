1a64c7a78f1f7bfddd3bc19e04d65d05f1443d74
anokata <quartenium@gmail.com>
2016-11-19 15:29:32 +0300

    euler, lang

--- zametki/source_stat.py
+++ zametki/source_stat.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import re
 
 def test ( a, b,  c , d,e):
     pass
@@ -32,6 +33,7 @@ def print_funcs(funcs):
 def _(fn='source_stat.py'):
     with open(fn, 'rt') as fin:
         lines = fin.readlines()
+        print(idents(lines))
         lines = list(filter(not_empty_line, lines))
         lines = list(filter(get_def, lines))
         funcs = dict()
@@ -40,6 +42,34 @@ def _(fn='source_stat.py'):
             fun_args = get_func_args(l)
             funcs[fun_name] = fun_args
         print_funcs(funcs)
+
+def validate_idents(idents):
+    without_nulls = filter(notnull, idents)
+
+#REFORM
+#def validate_idents idents:
+#    without_nulls = filter notnull idents
+#ENDREFORM
+
+def isnull(x): return x == 0
+def notnull(x): return not isnull(x)
+
+def idents(lines):
+    ident = list()
+    for l in lines:
+        spaces = get_ident(l)
+        ident.append(spaces)
+    return ident
+
+def get_ident(line):
+    return count_start_space(line)
+
+def count_start_space(line):
+    count = 0
+    for c in line:
+        if c != ' ':
+            return count
+        count += 1
 
 if __name__ == '__main__':
     _()
