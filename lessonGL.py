b37f1732d828519c3fe9ec7127775bf27a01f499
anokata <quartenium@gmail.com>
2016-11-27 17:06:25 +0300

    opengl font

--- lessonGL.py
+++ lessonGL.py
@@ -8,16 +8,71 @@ from PIL import Image
 ESCAPE = b'\033'
 window = 0
 
-def loadTexture(name):
+def initFont(name, w, h): # -> fontId
     image = Image.open(name)
     ix = image.size[0]
     iy = image.size[1]
     image = image.tobytes("raw", "RGBX", 0, -1)
-    #conver to BMP?
+    tid = texture_init(image, ix, iy)
+    col_count = ix // w
+    row_count = iy // h
+    return {'tid':tid,
+            'col':col_count,
+            'row':row_count,
+            'w':w,
+            'h':h,
+            'width':ix,
+            'height':iy,
+            }
+
+def font_coord_to_tex_coord(fid, x, y):
+    w = fid['w'] + 1
+    h = fid['h'] + 1
+    width = fid['width']
+    height = fid['height']
+    s, t = x, y
+    x *= w
+    y *= h
+    x = x/width
+    y = 1.0 - y/height
+    s = (s+1) * w 
+    t = (t+1) * w 
+    s = s/width
+    t = 1.0 - t/height
+    return x, t, s, y
+
+def say_2dfont(font_id, msg): 
+    tid = font_id['tid']
+    col = font_id['col']
+    glColor3f(1.0, 1.0, 1.0)
+    x = window_width
+    x /= 30
+    y = x
+    left = -40.0
+    for char in msg:
+        code = ord(char) - ord('a')
+        xx = code % col
+        yy = code // col
+        a, b, s, t = font_coord_to_tex_coord(font_id, xx, yy)
+        print(msg, char, code, x, a,b,s,t)
+        glBindTexture(GL_TEXTURE_2D, tid)   
+        glBegin(GL_QUADS)                   
+        glTexCoord2f(a, b)
+        glVertex3f(left-x, -y, 0.0)         
+        glTexCoord2f(a, t)
+        glVertex3f(left - x, y, 0.0)          
+        glTexCoord2f(s, t)
+        glVertex3f(left + x, y, 0.0)           
+        glTexCoord2f(s, b)
+        glVertex3f(left + x, -y, 0.0)          
+        glEnd()
+        left += 10
+
+def texture_init(data, w, h):
     t = glGenTextures(1)
     glBindTexture(GL_TEXTURE_2D, t)   
     glPixelStorei(GL_UNPACK_ALIGNMENT,1)
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
@@ -26,6 +81,16 @@ def loadTexture(name):
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
     glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE)
+    return t
+
+
+def loadTexture(name):
+    image = Image.open(name)
+    ix = image.size[0]
+    iy = image.size[1]
+    image = image.tobytes("raw", "RGBX", 0, -1)
+    #conver to BMP?
+    t = texture_init(image, ix, iy)
     return t
 
 
@@ -40,12 +105,14 @@ def InitGL(Width, Height):
     gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
     glMatrixMode(GL_MODELVIEW)
 
-    global texture, texturebg
+    global texture, texturebg, font
     texture = loadTexture('font0.png')
     texturebg = loadTexture('bg.png')
+    font = initFont('font0.png', 32, 32)
     glEnable(GL_TEXTURE_2D)
 
 
+font = 0
 window_width = 100.0
 w = h = 0
 
@@ -139,6 +206,8 @@ def DrawGLScene():
     glVertex3f(x, -x, 0.0)          
     glEnd()
 
+    say_2dfont(font, "abcde")
+
     drawBg()
 
     err = glGetError()
@@ -173,7 +242,6 @@ def keyPressed(*args):
 px = py = 0
 
 def mouse(button, state, x, y):
-    print(button, state)
     if button == GLUT_LEFT_BUTTON and state == GLUT_DOWN:
         a, b = glob_xy_calc(x,y)
         global px, py
@@ -182,7 +250,6 @@ def mouse(button, state, x, y):
         glutPostRedisplay()
 
 def motion(x, y):
-    print(x, y)
     glutPostRedisplay()
 
 def main():
@@ -203,5 +270,9 @@ def main():
     InitGL(640, 480)
     glutMainLoop()
 
-main()
-        
+if __name__=='__main__':
+    main()
+    f = initFont('font0.png', 10,10)
+    for i in range(12):
+        #print(font_coord_to_tex_coord(f, i, i))
+        pass
