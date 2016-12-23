c0682b0fb9e399282b09ae9ae6f9629eda30d4bb
anokata <anokata8@mail.ru>
2016-12-23 18:44:32 +0300

    div by hand emu

--- exercises/geron.py
+++ exercises/geron.py
@@ -10,24 +10,40 @@ def geron(x, e=10):
 print(geron(25))
 
 def finda(x):
+    print('finda', x)
     a = 0
     while not(a*a <= x and (a+1)**2 >x):
         a += 1
     return a
 
+def findap(x, b):
+    print('findap', x,b)
+    a = 0
+    while not((b+a)*a <= x and (b + a + 1)*(a+1) > x):
+        a += 1
+    return a
+
 def stolbik(x):
     res = ''
+    nx = 0
+    i = 0
     xx = str(x)
     if len(xx) % 2 == 1:
         xx = '0'+xx
     x = [x+y for x, y in zip(xx[::2], xx[1::2])]
-    print(x)
-    z = int(x[0])
-    a = finda(z)
-    res += str(a)
-    nx = int(x[0]) - a*a
-    nx = nx*100 + int(x[1])
-    print(a, nx)
-    b = res * 20
+    x += (['00']*100)
+    while nx !=0 or i==0:
+        c = nx*100 + int(x[i])
+        a = finda(c)
+        res += str(a)
+        nx = int(x[i]) - a*a
+        #c = nx*100 + int(x[i+1])
+        b = int(res) * 20
+        d = findap(c, b)
+        res += str(d)
+        nx = c - (b + d)*d
+        i += 2
+        print(nx, i, res)
+    print(res, int(res)**2)
 
 stolbik(69696)
