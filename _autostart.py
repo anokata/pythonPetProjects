42d6b38fdfc3fedbe6bdb61256f44b5d1d2b40c4
anokata <quartenium@gmail.com>
2016-12-25 13:55:20 +0300

    _autostart file day check, connection check

--- _autostart.py
+++ _autostart.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 #TODO: проверять первый запуск. записывать ежедневный файл, поставить в авто запуск и смотреть есть ли запись за сегодня и предлагать уже в зависимости.
 import os
+import time
 import subprocess
 exc = subprocess.getoutput
 import sys
@@ -11,12 +12,33 @@ sys.path.append(lib_dir)
 import currency
 import weather
 test = len(sys.argv) > 1
+#TODO if atel is off - turn. If time of statistic is not nowday then open, and weather simily
+
+def atel_status():
+    status = exc('nmcli connection show atel | grep GENERAL.STATE | cut -d: -f2 | tr -d [:blank:]')
+    return status == 'activated'
+
+def inet_on():
+    if not atel_status:
+        print('Подключаем интернет #1')
+        #exc('inet.sh')
+        exc('nmcli connection up atel')
+
+def is_new(filename):
+    now_day = time.localtime().tm_mday
+    file_day = time.localtime(os.path.getmtime('.')).tm_mday
+    return file_day == nowday
+
+def show_weather():
+    if not is_new('avansert_meteogram.png'):
+        pogoda()
+    #TODO
 
 def pogoda():
-    if (0,0,0) == weather.getWeather():
+    if (0,0,0) == weather.getWeather(weather.place):
         print("Нет погоды")
         return
-    temp, perc, wind = weather.getWeather()
+    temp, perc, wind = weather.getWeather(weather.place)
     print("Сегодняшняя средняя погда: T: %.1f  Осадки: %.1f  Ветер: %.1f м/c" % (temp, perc, wind))
     print("Запишу погоду на сегодня для статистики в ~/weather.cvs")
     dmy = exc('date +%d.%m.%Y')
@@ -41,21 +63,18 @@ def greating():
 
 daypart = greating()
 
-yn = '[y/n]:'
-r = input('Выполнить автозапуск? [y/n]')
-if r[0] != 'y':
-    print('Ну ладно...')
-    exit()
-print('Поехали!')
+def qry():
+    yn = '[y/n]:'
+    r = input('Выполнить автозапуск? [y/n]')
+    if r[0] != 'y':
+        print('Ну ладно...')
+        exit()
+    print('Поехали!')
 
 #TODO: сканирование всех разделов и монтирование в созданные каталоги
-print('Подключаем интернет #1')
-#exc('inet.sh')
-exc('nmcli connection up atel')
 #print('Монтируем раздел данных...')
 #exc('datamount.sh')
-#print('Подождём сек...')
-#exc('sleep 2')
+inet_on()
 if not test:
     print('Подождём сек...')
     exc('sleep 2')
@@ -76,4 +95,4 @@ if not test:
 
 exc('pday.py start')
 
-print('Удачного %s!' % daypart)
+print('Удачного %s! vim txthub/live.org' % daypart)
