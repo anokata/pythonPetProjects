594d2e82f3a798b9868a356a49a3839240c91516
anokata <quartenium@gmail.com>
2017-01-18 16:21:11 +0300

    traing

---
+++ exercises/count4.py
@@ -0,0 +1,23 @@
+xrange = range
+def solution(A):
+    n = len(A)
+    A.sort()
+    if A[0] > 1:
+        return 1
+    if n == 1:
+        if A[0] <=0 or A[0] > 1:
+            return 1
+        else:
+            return 2
+    print(A, A[-1])
+    for i in xrange(n-1):
+        if A[i] < 0 and A[i+1] > 1:
+            return 1
+        elif A[i] > 0 and A[i+1] - A[i] > 1:
+            return A[i] + 1
+    return A[-1] + 1
+    
+            
+            
+        
+    
