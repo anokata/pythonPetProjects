f7de582f32982aca6ed817b7cb24ca9b000e4693
anokata <quartenium@gmail.com>
2017-02-11 18:36:04 +0300

    Prepare ui for openid login, chg templates, add providers.

---
+++ flask_learn/config.py
@@ -0,0 +1,6 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'f35fc593c73a35956a70d0a7eeac9bdb'
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' }
+]
