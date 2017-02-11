3e5a2f31ed399bde2dd9a52d4c01347a9074384f
anokata <quartenium@gmail.com>
2017-02-11 18:06:12 +0300

    Forms add, login form, index page.

--- flask_learn/app1.py
+++ flask_learn/app1.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, flash, redirect
 from flask import request
 from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import TextField, BooleanField
+from wtforms.validators import Required
+
 app = Flask(__name__)
+app.config.from_object('config')
 
 records = {
         'user1': {
@@ -13,20 +18,31 @@ records = {
         }
 }
 
+class LoginForm(FlaskForm):
+    openid = TextField('openid', validators = [Required()])
+    remember_me = BooleanField('remember_me', default = False)
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + 
+              form.openid.data + '", remember_me=' + 
+              str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+                            title = 'Sign In',
+                            form = form)
+
 @app.route("/")
 def root():
     return "<h1>x</h1>"
 
 @app.route("/index")
 @app.route("/<dir>-<int:num>/", methods=['GET', 'POST'])
-def sub_root(dir, num):
-    if request.method == 'POST':
-        return 'POST'
-    else:
-        name_arg = request.args.get('dir', 'nodir')
-        return ' '.join(("<h2> dir:", dir, "_", str(num), 
-                         'NAME: ', name_arg,
-                         "</h2>"))
+def sub_root(dir='', num=''):
+    name_arg = request.args.get('dir', 'nodir')
+    return render_template('index.html', name=name_arg)
 
 @app.route("/template1/", methods=['POST', 'GET'])
 def templ1():
