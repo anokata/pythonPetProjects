2e7bd1e88b2d2aca075cc19bbbca1497fa092690
anokata <quartenium@gmail.com>
2017-02-11 13:50:35 +0300

    start flask relearning

--- exercises/factor101.py
+++ exercises/factor101.py
@@ -1,11 +1,18 @@
 xrange = range
 from math import sqrt
 def solution(N):
+    import math
     if N in [1, 2, 3]:
         return N
+<<<<<<< Updated upstream
     n = int(sqrt(N)) + 1
     c = 2
     for i in xrange(2, n):
+=======
+    n = int(math.sqrt(N)) + 1
+    c = 1
+    for i in xrange(1, n):
+>>>>>>> Stashed changes
         if not N % i:
             c += 2
     if n * n == N:
