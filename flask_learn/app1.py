6a965513bf1d37eed719d9716bb25487ccc73266
anokata <quartenium@gmail.com>
2017-02-12 09:54:52 +0300

    add mirgate

--- flask_learn/app1.py
+++ flask_learn/app1.py
@@ -20,6 +20,18 @@ records = {
         }
 }
 
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+    role = db.Column(db.SmallInteger, default = ROLE_USER)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
 class LoginForm(FlaskForm):
     openid = TextField('openid', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
@@ -38,9 +50,6 @@ def login():
                             providers = app.config['OPENID_PROVIDERS'])
 
 @app.route("/")
-def root():
-    return "<h1>x</h1>"
-
 @app.route("/index")
 @app.route("/<dir>-<int:num>/", methods=['GET', 'POST'])
 def sub_root(dir='', num=''):
--- flask_learn/require_install.sh
+++ flask_learn/require_install.sh
@@ -1,3 +1,4 @@
 pip3 install flask
 pip3 install flask-wtf
 pip3 install flask-sqlalchemy
+pip3 install sqlalchemy-migrate
