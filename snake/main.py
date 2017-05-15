27117fca61dffc63bfef8fa2a1a817bf08bb1e96
anokata <anokata8@mail.ru>
2017-05-15 17:47:49 +0300

    reclean

--- snake/main.py
+++ snake/main.py
@@ -4,7 +4,6 @@ from OpenGL.GLU import *
 import sys
 sys.path.append('../modules')
 import random
-from ByteFont import *
 from gl_main import *
 import stateSystem
 import ByteFont
@@ -16,34 +15,16 @@ state = State()
 state.world = State()
 
 def init():
-    state.font = init_font(ByteFont.font_file, 16, 16)
-    state.font10x16 = init_font(ByteFont.font_file10x16, 10, 16)
-    set_fonts(state.font10x16, state.font)
+    state.font = ByteFont.init_font(ByteFont.font_file, 16, 16)
+    state.font10x16 = ByteFont.init_font(ByteFont.font_file10x16, 10, 16)
+    ByteFont.set_fonts(state.font10x16, state.font)
     pass
 
-def gl_draw_pre():
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()                    
-    glDisable(GL_CULL_FACE)             
-    glShadeModel(GL_SMOOTH)
-    glEnable(GL_TEXTURE_2D)
-    glEnable(GL_BLEND)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-    glDisable(GL_DEPTH_TEST)
-    glColor4f(0, 1, 1, 0.5)
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-
-def gl_error_msg():
-    err = glGetError()
-    if err:
-        print(err, gluErrorString(err))
-    glutSwapBuffers()
-
 def DrawGLScene():
-    gl_draw_pre()
+    gl_draw_pre_2d()
     stateSystem.handleEvent('draw', state.world)
     #draw(state.world)
-    draw_chars_tex("hi", y=1, x=1, color=(0,1,0))
+    ByteFont.draw_chars_tex("hi", y=1, x=1, color=(0,1,0))
     gl_error_msg()
 
 def keyPressed(*args):
@@ -64,10 +45,10 @@ def mouse(button, state, x, y):
 def motion(x, y):
     pass
 
-def step():
+def step(x):
     pass
 
-def ReSizeGLScene(Width, Height):
+def ReSizeGLScene_2d(Width, Height):
     state.w = w = Width
     state.h = h = Height
     if Height == 0:                     
@@ -87,7 +68,7 @@ def main():
     state.window = glutCreateWindow("")
     glutDisplayFunc(DrawGLScene)
     #glutIdleFunc(DrawGLScene)
-    glutReshapeFunc(ReSizeGLScene)
+    glutReshapeFunc(ReSizeGLScene_2d)
     glutKeyboardFunc(keyPressed)
     glutTimerFunc(100, step, 1)
     InitGL(640, 480)
@@ -96,3 +77,5 @@ def main():
 
 if __name__=='__main__':
     main()
+
+#TODO: make decor for just needed functions. separate.
