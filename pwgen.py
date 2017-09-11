c06de87c53a8e42660b885de9d14796cc928c687
anokata <anokata8@mail.ru>
2017-09-11 21:55:14 +0300

    add pwgen

---
+++ pwgen.py
@@ -0,0 +1,9 @@
+for a in range(10):
+    for b in range(10):
+        for c in range(10):
+            for d in range(10):
+                for e in range(10):
+                    for f in range(10):
+                        for g in range(10):
+                            for h in range(10):
+                                print("{}{}{}{}{}{}{}{}".format(a,b,c,d,e,f,g,h))
