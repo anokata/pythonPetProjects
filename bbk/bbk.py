504042031db97a3a99f447ef0cfcc3cf561b481b
tikhomirovsvl <tikhomirovsvl@mosreg.ru>
2017-12-18 16:02:17 +0300

    [bbk] [add]

---
+++ bbk/bbk.py
@@ -0,0 +1,19 @@
+# symbols: letters a-z, A-Z, + ! * #
+# + is disjunction
+# ! is "not"
+# # is tau
+# * is square symbol
+# #1 *1 - connections
+
+# 1. Introducing contractile symbols
+# i.e. => as +!
+
+symcom1 = "+ab"
+
+def is_term(s):
+    return False
+
+def is_corr(s):
+    return False
+
+
