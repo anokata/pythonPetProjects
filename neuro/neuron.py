8f7fa8f326ae484558f6b2df3c682dcaf5be1e5e
anokata <anokata8@mail.ru>
2020-02-12 10:41:19 +0300

    nl

--- neuro/neuron.py
+++ neuro/neuron.py
@@ -1,15 +1,21 @@
 class Neuron:
     w = [] # weights
+
+    def __init__(self, n):
+        self.w = [0.0] * n
     
-    def input(x):
+    def input(self, x):
         s = 0
+        w = self.w
         for i in range(0, len(x)):
             s += x[i] * w[i]
-        return F(s)
+        return self.F(s)
 
 
-    def F(s):
+    def F(self, s):
         return s
 
-n = Neuron()
+n = Neuron(10)
+print(n.w)
+print(n.input([1,2,3,4,5,6,7,8,9,10]))
 
